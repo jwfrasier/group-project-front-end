c732779098f631ecdeab5c1f0292a35c305bdb65
Ayodeji Fashola <fashola2@Ayodejis-MacBook-Pro.local>
2017-10-10 13:32:10 -0500

    made changes to home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+
 
 class Home extends Component {
   render() {
     return (
-      <div>
-      <p>home</p>
-      </div>
+      <div className='container'>
+      <img src="https://i.ytimg.com/vi/w6DW4i-mfbA/hqdefault.jpg" />
+      <h1>Welcome to Who Dat</h1>
+      <nav>
+        <div className='nav-buttons'>
+          <a href="login">Log-In</a>
+          <a href="register">Register</a>
+          <a href="lost">Forgot Username/Password?</a>
+        </div>
+      </nav>
+
+  </div>
     );
   }
 }
